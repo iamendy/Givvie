978c5e6564208168bebd92bbdbd148a7847df220
Nnamdi <17215830+iamendy@users.noreply.github.com>
2023-09-18 13:31:04 +0100

    update: Add navbar and footer

---
+++ src/pages/_app.tsx
@@ -0,0 +1,60 @@
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import type { AppProps } from "next/app";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { lineaTestnet } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+import { useEffect, useState } from "react";
+import { Inter } from "next/font/google";
+import Layout from "../components/Layout";
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [lineaTestnet],
+  [publicProvider()]
+);
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--inter",
+});
+
+const projectId = process.env.NEXT_PUBLIC_WC_PROJECT_ID as string;
+
+const { connectors } = getDefaultWallets({
+  appName: "Givvie",
+  projectId: projectId,
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+  webSocketPublicClient,
+});
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setIsLoaded(true);
+  }, []);
+
+  return (
+    <main className={`${inter.variable} overflow-hidden`}>
+      {isLoaded && (
+        <WagmiConfig config={wagmiConfig}>
+          <RainbowKitProvider chains={chains} modalSize="compact">
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </RainbowKitProvider>
+        </WagmiConfig>
+      )}
+    </main>
+  );
+}
+
+export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return <main>Givvie app</main>;
+};
+
+export default Home;
